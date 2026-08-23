4cb8d9318904225230b0db07b0d559ea9d11b318
SerenitySTR <serenityr9a@gmail.com>
2026-08-23 04:02:10 +0300

    Update registration.js

--- assets/JavaScript/registration.js
+++ assets/JavaScript/registration.js
@@ -139,10 +139,3 @@ registerButton.addEventListener("click", () => {
         data
     );
 });
-
-if (window.cef && typeof window.cef.on === "function") {
-    window.cef.on("registration:show", (data) => {
-        alert("SHOW: " + data);
-        showRegistration();
-    });
-}
