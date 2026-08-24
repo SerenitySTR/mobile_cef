287aace516dae1ebea073ab987542e2a3e61672e
SerenitySTR <serenityr9a@gmail.com>
2026-08-24 12:40:12 +0300

    upd

---
+++ assets/JavaScript/authorization.js
@@ -0,0 +1,56 @@
+const authorization = document.getElementById("authorization");
+
+const authorizationUsernameInput = document.getElementById("authorization-username");
+const authorizationPasswordInput = document.getElementById("authorization-password");
+
+const authorizationButton = document.getElementById("authorization-button");
+const authorizationPasswordEye = document.querySelector(".authorization-password-eye");
+
+function showAuthorization() {
+    authorization.classList.add("active");
+}
+
+function hideAuthorization() {
+    authorization.classList.remove("active");
+}
+
+authorizationPasswordEye.addEventListener("click", () => {
+    authorizationPasswordInput.type = authorizationPasswordInput.type === "password"
+        ? "text"
+        : "password";
+});
+
+authorizationButton.addEventListener("click", () => {
+    const password = authorizationPasswordInput.value;
+
+    if (!password) {
+        showError("Помилка", "Введіть пароль.");
+        return;
+    }
+
+    if (password.length < 6 || password.length > 21) {
+        showError("Невірний пароль", "Пароль повинен містити від 6 до 21 символу.");
+        return;
+    }
+
+    if (!/^[A-Za-z0-9_]+$/.test(password)) {
+        showError("Невірний пароль", "Пароль може містити лише латинські літери, цифри та знак _.");
+        return;
+    }
+
+    GameCef.sendJson("authorization:submit", {
+        username: authorizationUsernameInput.value.trim(),
+        password: password
+    });
+});
+
+GameCef.on("authorization:show", (data) => {
+    authorizationUsernameInput.value = data;
+    authorizationPasswordInput.value = "";
+
+    showAuthorization();
+});
+
+GameCef.on("authorization:hide", () => {
+    hideAuthorization();
+});
