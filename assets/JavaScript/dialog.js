e4a181e17f6f28044fb452d9fec404e821bcaa4d
SerenitySTR <serenityr9a@gmail.com>
2026-09-15 18:33:15 +0300

    upd

--- assets/JavaScript/dialog.js
+++ assets/JavaScript/dialog.js
@@ -298,7 +298,7 @@ var Dialog = {
         var response = {
             DialogId: this.dialogId,
             ButtonId: buttonId,
-            Input: encodeURIComponent(value),
+            Input: value,
             SelectedIndex: this.selectedIndex,
             SelectedItemId: this.selectedItemId
         };
