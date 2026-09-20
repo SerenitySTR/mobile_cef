9df4fbdd25983fbafe377f1c5acf97e7672e986a
SerenitySTR <serenityr9a@gmail.com>
2026-09-21 01:40:55 +0300

    Update admin-panel.js

--- assets/JavaScript/admin-panel.js
+++ assets/JavaScript/admin-panel.js
@@ -418,12 +418,8 @@ var AdminPanel = {
         if(oldTransferLayer)oldTransferLayer.remove();
 
         if(this.transferOpen) {
-            if(this.IsMobileLandscape()) {
-                this.RenderMobileTransfer();
-            } else {
-                var self=this;
-                requestAnimationFrame(function(){ self.PositionTransferMenu(); });
-            }
+            var self=this;
+            requestAnimationFrame(function(){ self.PositionTransferMenu(); });
         }
     },
     RenderMobileTransfer: function() {
@@ -501,7 +497,7 @@ var AdminPanel = {
             controls='<div class="admin-composer">'
             +'<div class="admin-ticket-controls">'
             +'<div class="admin-quick-wrap"><button type="button" class="admin-quick-toggle" data-admin-quick-toggle>Швидкі відповіді '+(this.quickOpen?'▴':'▾')+'</button>'+quickMenu+'</div>'
-            +'<div class="admin-transfer-row"><div class="admin-transfer-select"><button type="button" class="admin-select admin-transfer-toggle" data-admin-transfer-toggle>'+this.TransferAdminLabel(admins)+'</button>'+(this.transferOpen&&!this.IsMobileLandscape()?'<div class="admin-transfer-menu">'+(admins.length?admins.map(function(a){return '<button type="button" class="admin-transfer-option '+(String(self.transferAdminId)===String(a.id)?"active":"")+'" data-admin-transfer-option="'+self.Escape(a.id)+'">'+self.Escape(a.name)+' ['+self.Escape(a.id)+']</button>';}).join(""):'<div class="admin-transfer-empty">Немає адміністраторів онлайн</div>')+'</div>':'')+'</div><button type="button" data-admin-action="transfer">Передати</button><button type="button" data-admin-action="release">Звільнити</button></div>'
+            +'<div class="admin-transfer-row"><div class="admin-transfer-select"><button type="button" class="admin-select admin-transfer-toggle" data-admin-transfer-toggle>'+this.TransferAdminLabel(admins)+'</button>'+(this.transferOpen?'<div class="admin-transfer-menu">'+(admins.length?admins.map(function(a){return '<button type="button" class="admin-transfer-option '+(String(self.transferAdminId)===String(a.id)?"active":"")+'" data-admin-transfer-option="'+self.Escape(a.id)+'">'+self.Escape(a.name)+' ['+self.Escape(a.id)+']</button>';}).join(""):'<div class="admin-transfer-empty">Немає адміністраторів онлайн</div>')+'</div>':'')+'</div><button type="button" data-admin-action="transfer">Передати</button><button type="button" data-admin-action="release">Звільнити</button></div>'
             +'</div>'
             +'<div class="admin-send-row"><textarea class="admin-textarea" data-admin-draft placeholder="Напишіть повідомлення…"></textarea><button type="button" class="admin-send admin-primary" data-admin-action="send">Надіслати</button></div>'
             +'<button type="button" class="admin-close-ticket" data-admin-action="resolve">Закрити тікет</button>'
